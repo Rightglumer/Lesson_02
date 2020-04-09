f72280292381b156867cc8ce8be9577cad3b00f5
glumer <glumer@bk.ru>
2020-04-09 19:36:25 +0300

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] getRandomArray(){
+
+    }
+
+    public static int[] getIntArray(int maxArraySize){
+        double arraySize;
+        arraySize = Math.random();
+        int[]
+    }
+}
